3d96a6f1f11030ff5e5661c5fce64435fce6dafb
valls <thiago@agenciatw.com.br>
2019-12-18 13:13:00 -0300

    ending interface concepts

--- src/structs/interface/main.go
+++ src/structs/interface/main.go
@@ -6,6 +6,8 @@ type EntityInterface interface {
 	start() string
 }
 
+type Names []interface{}
+
 type Entity struct {
 	Name string
 	Year int
@@ -34,26 +36,48 @@ func startEntity(e EntityInterface) string {
 	return e.start()
 } 
 
+func (n *Names) load() {
+	*n = Names{
+		"Nair",
+		"Paulo",
+		"Thiago",
+		"ANdre",
+	}
+}
+
+func (n Names) printNames() {
+	fmt.Println(n)
+}
+
 func main() {
-	car := Car{
-		Entity: Entity {
-			Name: "Corolla",
-			Year: 2020,
-			Color: "verde",
-		},
-		Type: "Carro",
-	}
+	// car := Car{
+	// 	Entity: Entity {
+	// 		Name: "Corolla",
+	// 		Year: 2020,
+	// 		Color: "verde",
+	// 	},
+	// 	Type: "Carro",
+	// }
 
-	moto := Motorcycle{
-		Entity: Entity {
-			Name: "CG150",
-			Year: 2020,
-			Color: "vermelha",
-		},
-		Type: "Moto",
-	}
+	// moto := Motorcycle{
+	// 	Entity: Entity {
+	// 		Name: "CG150",
+	// 		Year: 2020,
+	// 		Color: "vermelha",
+	// 	},
+	// 	Type: "Moto",
+	// }
 
-	fmt.Println(startEntity(car))
-	fmt.Println(startEntity(moto))
+	// fmt.Println(startEntity(car))
+	// fmt.Println(startEntity(moto))
+
+	var names Names
+	var outros Names
+
+	names.load()
+	names.printNames()
+
+	outros.load()
+	outros.printNames()
 }
 
